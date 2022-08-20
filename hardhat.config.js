6ccd3c4f41bbad5093969ce72e8be6bfa3645caf
Caleb Lent <caleb@stcroixwebagency.com>
2022-08-20 06:02:55 -0500

    added account and rinkeby url

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.11",
-};
+  networks: {
+    url: process.env.RINKEBY_URL,
+    accounts: [process.env.PRIVATE_KEY],
+  },
+}
