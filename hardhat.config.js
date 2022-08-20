5b42b483e1980efcd37ab0e43bf4a292e2b41c52
Caleb Lent <caleb@stcroixwebagency.com>
2022-08-20 14:43:06 -0500

    deployed + minted NFTs with deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,9 @@ require("dotenv").config()
 module.exports = {
   solidity: "0.8.11",
   networks: {
-    url: process.env.RINKEBY_URL,
-    accounts: [process.env.PRIVATE_KEY],
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 }
